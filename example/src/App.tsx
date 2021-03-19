bcbdd00b21367d60838cb746a846f3a17eef6541
Danik <daniknewgarden@gmail.com>
2021-03-19 21:31:53 +0200

    Inited storybook

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-import { ExampleComponent } from 'exo-design-system'
+import { ExampleComponentStyled } from 'exo-design-system'
 import 'exo-design-system/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponentStyled text='Create React Library Example 😄' />
 }
 
 export default App
