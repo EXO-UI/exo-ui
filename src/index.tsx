9aca2648fe6dc3d2068ab926a299ce06d6a52840
Danik <daniknewgarden@gmail.com>
2021-03-19 21:26:40 +0200

    Added styled-components

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react'
-import styles from './styles.module.css'
+import styled from 'styled-components'
 
 interface Props {
   text: string
+  className?: string
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const ExampleComponent = ({ text, className }: Props) => {
+  return <div className={className}>Hello world {text}</div>
 }
+
+export const ExampleComponentStyled = styled(ExampleComponent)`
+  background-color: tomato;
+  margin: 2em;
+  padding: 0.5em;
+  border: 2px solid #000;
+  font-size: 2em;
+  text-align: center;
+`
