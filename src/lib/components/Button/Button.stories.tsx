525e936e3ca330be7f4d02311a4c39457f7c47ac
Daniel Novosad <work.daniknewgarden@gmail.com>
2023-08-09 17:01:51 +0200

    chore: #2 set up storybook

---
+++ src/lib/components/Button/Button.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button } from "./Button";
+
+const meta = {
+  title: "Test/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    onClick: () => alert("TEST"),
+  },
+};
