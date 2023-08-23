018332c961957a925fbe874133caa5425532e043
Daniel Novosad <work.daniknewgarden@gmail.com>
2023-08-23 23:15:12 +0200

    build: add emotion styling solution
    - it is used together with @emotion/react css prop
    - to add css prop update swc plugins with @swc/plugin-emotion
    
     #12

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -1,7 +1,18 @@
+import { css } from "@emotion/react";
+
 interface ButtonProps {
   onClick(): void;
 }
 
+const styles = css`
+  color: #fafafa;
+  background: #000000;
+`;
+
 export function Button({ onClick }: ButtonProps) {
-  return <button onClick={onClick}>Click me</button>;
+  return (
+    <button css={styles} onClick={onClick}>
+      Click me
+    </button>
+  );
 }
