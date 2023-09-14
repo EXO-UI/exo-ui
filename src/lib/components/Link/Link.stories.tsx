3199b726b37fdaa131fdd88e81de83746d94dd57
Jan Szewczyński <48333162+lulek1410@users.noreply.github.com>
2023-09-14 21:56:35 +0200

    feat(link): introduce new Link component (#33)
    BREAKING CHANGE: New Link component
    
    re #27

---
+++ src/lib/components/Link/Link.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Link } from "./Link";
+
+const meta = {
+	title: "Base/Link",
+	component: Link,
+	parameters: {
+		layout: "centered",
+	},
+	argTypes: {
+		children: {
+			description: "The content of the link.",
+		},
+	},
+	tags: ["autodocs"],
+} satisfies Meta<typeof Link>;
+
+type Story = StoryObj<typeof meta>;
+export const Default: Story = {
+	args: {
+		children: "Default",
+	},
+};
+
+export default meta;
