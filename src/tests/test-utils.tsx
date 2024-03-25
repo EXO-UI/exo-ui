4055667ca3c326a798aee490fcc0fcbbeceaeac5
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-03-25 22:40:45 +0100

    test: add emotion tests tool

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -1,7 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
 import { PropsWithChildren, ReactElement } from "react";
 import { render, RenderOptions } from "@testing-library/react";
+import { matchers } from "@emotion/jest";
 import { ThemeProvider } from "../lib";
+
+// Add custom matchers for testing emotion styles - see https://emotion.sh/docs/@emotion/jest#tohavestylerule
+expect.extend({ ...matchers });
 
 function AllTheProviders({ children }: PropsWithChildren<unknown>) {
   return <ThemeProvider>{children}</ThemeProvider>;
