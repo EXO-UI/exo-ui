cfe82fb44a101210db98ec2387161a89a5800ca3
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-03-25 19:01:30 +0100

    build(storybook): update storybook to v8
    update storybook version, add a new visual tests runner

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,7 +5,6 @@ import "./global.css";
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
         color: /(background|color)$/i,
