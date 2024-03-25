465e07186acf7c6d64f5817db7384065cb332863
Daniel Novosad <work.daniknewgarden@gmail.com>
2024-03-25 22:40:45 +0100

    test: add emotion tests tool

--- src/lib/components/Stack/Stack.test.tsx
+++ src/lib/components/Stack/Stack.test.tsx
@@ -20,10 +20,9 @@ describe("StackBox", () => {
   test("Should render with custom level", () => {
     const { getByText } = render(<StackBox level={2}>Some content</StackBox>);
 
-    // emotion removes unusable spaces from strings
-    expect(getByText("Some content")).toHaveStyle(
-      // TODO: fix this weird condition - use some emotion tools https://emotion.sh/docs/@emotion/jest
-      `box-shadow: ${boxShadows[2].replace(/,\s*0px/g, ",0px")}`,
+    expect(getByText("Some content")).toHaveStyleRule(
+      "box-shadow",
+      boxShadows[2].replace(/,\s*0px/g, ",0px"), // emotion removes unused spaces
     );
   });
 });
